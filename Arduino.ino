03520f329091c048b0a7673975d2f03f1c05d08d
kilotwo <531970615@qq.com>
2019-02-01 19:58:02 +0800

    Arduino

---
+++ Arduino.ino
@@ -0,0 +1,318 @@
+/*
+    每个轮相连的步进电机按下图与步进电机扩展版连接
+           车头
+   X:0--------------2:Z
+     |              |
+     |              |
+     |              |
+   Y:1--------------3:A
+           车尾
+    各电机与扩展版的每个插槽的连接顺序为:
+    从上至下: 蓝, 红, 绿, 黑
+*/
+#include <ServoTimer2.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <MultiLCD.h>
+//Sensor
+int debug_sensor = A3; //程序触发传感器
+
+ServoTimer2 m_Servo[5]; 
+
+#define MOVE_DBG  //底盘行走测试,前进, 后退, 左转, 右转, 左平移, 右平移
+LCD_SSD1306 lcd; //OLED
+
+//Servo
+char angle_return[100];
+const int frequency = 25; 
+const int speedd = 25;
+const int actionTimes = 2000;
+int servo_port[5] = {22, 24, 26, 28, 29}; //定义机械臂各舵机引脚，基座至机械爪
+float servo_angle[5] = {73, 112, 31, 113, 90}; //定义机械臂各舵机初始角度
+const int servo_num = sizeof(servo_port) / sizeof(servo_port[0]); //定义舵机数量
+//Path
+static String data = ""; //存储二维码顺序
+int order[2];  //存储抓取及放置的顺序
+//====================================+变量定义枚举类型+=======================================
+enum Direction{MOVE_FORWARD= 1, MOVE_BACK, MOVE_LEFT, MOVE_RIGHT, TURN_RIGHT, TURN_LEFT};
+enum Cmd{QR=1, COLOR};
+
+struct XY_POS{
+  double x;
+  double y;
+  double theta;
+}pos;
+//====================================-变量定义-=======================================
+void setup() {        //初始化
+  Serial.begin(9600);
+   pinMode(debug_sensor, INPUT);
+     LcdInit();
+  lcd.begin();
+  lcd.clear();
+   ServoInit();
+  delay(1000);
+  initMotors();
+  XYStop();
+} 
+ /*
+  moveTo(-0.5, 0, true, MOVE_LEFT); //右平移
+  moveTo(0.5, 0, false, MOVE_RIGHT); //左平移
+  moveTo(0, 0.5, false, MOVE_FORWARD); //前进
+  moveTo(0, -0.5, false, MOVE_BACK); //后退
+  */
+  
+void loop() 
+{
+#ifdef MOVE_DBG
+ // moveTo(0.4, 0, false, MOVE_RIGHT); //左平移
+   moveTo(0, 1.0, false, MOVE_FORWARD); //前进
+   //moveTo(-0.4, 0, true, MOVE_LEFT); //右平移
+//   moveTo(0, -0.4, false, MOVE_BACK); //后退
+   delay(1000);
+   QRDetect();                      //此时得到二维码顺序   
+   delay(1000);
+    moveTo(-0.7, 0, false, MOVE_LEFT); //右平移
+  Colour()   ;                 //颜色检测
+  // 根据抓取顺序进行物料抓取  1：left ；2：right
+  Putt();
+    delay(1000);
+  moveTo(0, -0.5, false, MOVE_BACK); //后退 
+ 
+  
+#endif
+  // if(!digitalRead(debug_sensor))
+  //  SendCmd(QR);
+  while(1);
+}
+void serialEvent(){
+  while(Serial.available())
+    Serial.read();
+}
+//colour显示函数
+void LcdDisplay(String data){
+  lcd.clear();
+  lcd.setCursor(0, 2);
+  lcd.print(data);
+}
+
+/*
+  舵机初始化函数
+*/
+void ServoInit(){
+  for(int i=0;i<servo_num;i++){
+    m_Servo[i].attach(servo_port[i]);
+    ServoGo(i, servo_angle[i]);
+  }
+}
+ // OLED初始化函数
+
+void LcdInit(){
+  lcd.begin();
+  lcd.clear();
+}
+//====================================+OLED显示函数+=====================================
+
+//函数名: LcdDisplay();
+//mode:QR 显示二维码识别结果
+void LcdDisplayer(String datas){
+    lcd.clear();
+  lcd.setCursor(0, 3);
+  lcd.printLong(datas.toInt(), FONT_SIZE_LARGE);
+}
+
+
+/*
+  舵机角度最大值及最小值检测函数
+  小于最小值, 返回最小值
+  大于最大值, 返回最大值
+*/
+float ValueCompare(float value){
+  if(value <= 0) 
+    return 0;
+  else if(value >= 180)
+    return 180;
+  else 
+    return value;
+}
+
+/*
+  舵机角度转换为PWM函数
+  参数为: 舵机角度
+  返回值: PWM
+*/
+float Angle2Pwm(float angle){
+  return map(ValueCompare(angle), 0, 180, 500, 2500);
+}
+
+void ServoGo(int which, float angle){
+  m_Servo[which].write(Angle2Pwm(angle));
+}
+
+void ServoMove(float * pCurrent, float * pTarget){
+  float diff[servo_num];
+  for(int i=0;i<servo_num;i++)
+    diff[i] = (*(pTarget+i) - *(pCurrent+i)) == 0 ? 0 : (*(pTarget+i) - *(pCurrent+i)) / frequency;
+  for(int i=0;i<frequency;i++){
+    for(int j=0;j<servo_num;j++){
+      *(pCurrent+j) += diff[j];
+      ServoGo(j, *(pCurrent+j));
+    }
+    delay(speedd);
+  }
+}
+
+void ServoSet(int count, ...){		
+  float targetAngle[count];
+  va_list ap;
+  va_start(ap, count);
+  for(int i=0;i<count;i++)
+    targetAngle[i] = va_arg(ap, int);
+  va_end(ap);
+  ServoMove(servo_angle, targetAngle);
+  delay(1000);
+}
+void  Colour()
+{
+  
+    // 颜色检测
+  ServoSet(servo_num, 73,61,71,85,83); //机械臂移动至第一个物料处
+  ServoSet(servo_num, 73,61,54,82,83); //机械臂移动至第一个物料处
+  ServoSet(servo_num, 55,61,54,82,83); //机械臂移动至第一个物料处
+   SendCmd(COLOR);                     //检测放置台左侧物料颜色
+  ServoSet(servo_num,107,61,54,82,83); //机械臂移动至第二个物料处
+ SendCmd(COLOR); //检测放置台右侧物料颜色
+  
+  ServoSet(servo_num,73, 112, 31, 113, 90); //机械臂复位
+    delay(1000);
+      String serialData = SendCmd(COLOR); //获取颜色检测结果
+      LcdDisplayer(serialData);        //颜色检测结果
+ //      delay(1000);
+   // LcdDisplayer(serialData);        //颜色检测结果
+    OrderSet(serialData);          //排序结果
+    delay(1000);
+  
+}
+
+//机械臂放置
+void Putt(){
+  //根据抓取顺序进行物料抓取  0：left ；1：right
+ // for(int i=0;i<2;i++)
+  //{
+    switch(order[0])
+    {
+      case 0:
+        //get 0    left
+        ServoSet(servo_num, 73,74,42,82,90);  //抓取1
+        ServoSet(servo_num, 43,74,42,82,90);  //抓取2
+        ServoSet(servo_num, 48,67,40,82,100);  //抓取3
+           ServoSet(servo_num, 48,67,39,30,100);  //抓取4
+              ServoSet(servo_num, 48,67,5,40,100);  //抓取5
+                 ServoSet(servo_num, 48,55,20,40,100);  //抓取6
+                    ServoSet(servo_num, 48,40,42,48,100);  //抓取7
+        ServoSet(servo_num,48,40,42,48,72);    //抓紧1
+        ServoSet(servo_num, 48,64,61,80,72);    //抓紧1中间
+        ServoSet(servo_num,125,64,61,80,72);    //抓紧2中间
+         moveTo(-0.5, 0, false, MOVE_LEFT); //右平移
+         
+        ServoSet(servo_num, 125,64,61,80,100);  //松开1
+        
+            moveTo(0.5, 0, false, MOVE_LEFT); // left平移
+      break;
+      case 1:  
+        //get 1     right
+         ServoSet(servo_num, 73,74,42,82,90);  //抓取1
+        ServoSet(servo_num, 100,74,42,82,90);  //抓取2
+        ServoSet(servo_num, 100,67,40,82,100);  //抓取3
+           ServoSet(servo_num, 100,67,39,30,100);  //抓取4
+              ServoSet(servo_num, 100,67,5,40,100);  //抓取5
+                 ServoSet(servo_num, 100,55,20,40,100);  //抓取6
+                    ServoSet(servo_num, 100,40,42,48,100);  //抓取7
+      ServoSet(servo_num,100,40,42,48,72);    //抓紧1
+        ServoSet(servo_num,100,64,61,80,72);    //抓紧1中间
+        ServoSet(servo_num,125,64,61,80,72);    //抓紧2中间
+         moveTo(-0.5, 0, false, MOVE_LEFT); //右平移
+         delay(2000);
+        ServoSet(servo_num, 125,64,61,80,100);  //松开1
+        delay(2000);
+            moveTo(0.5, 0, false, MOVE_LEFT); //left平移
+    /*    
+        ServoSet(servo_num, 13, 80, 42, 90, 105);
+        ServoSet(servo_num, 13, 90, 38, 90, 105);
+        ServoSet(servo_num, 13, 130, 71, 78, 105);
+        ServoSet(servo_num, 13, 130, 71, 78, 53);
+        ServoSet(servo_num, 13, 80, 42, 90, 53);
+        ServoSet(servo_num, 93, 80, 42, 90, 53);
+        */
+      break;
+    }
+     ServoSet(servo_num, 73,61,71,85,83); //2.机械臂复位
+}
+
+//==================================-二维码检测-======================================
+
+/******************************串口读取及数据处理部分********************************/
+/*
+ * 串口读取函数
+ * 返回值类型：字符串
+ */
+String SerialRead(){
+  String incomingStr = "";
+  while(true){
+    while(Serial.available()){
+      incomingStr = Serial.readStringUntil('\n');
+    }
+    if(incomingStr != "")
+      break;
+  }
+  return incomingStr;
+}
+
+/*
+
+//串口发送二维码识别及颜色检测命令
+//mode:检测模式
+//mode=QR:检测二维码
+//mode=COLOR:检测颜色
+ */
+String SendCmd(int mode){
+  switch(mode){
+    case QR: Serial.print("qr\n"); break;
+    case COLOR: Serial.print("get\n"); break;
+  }
+  String cmd = SerialRead();
+  if(cmd != "error"){
+    
+   // LcdDisplay(cmd);
+    return cmd;
+  }
+  else{
+    //此处可以通过控制机械臂来调整摄像头，使其能够检测到目标
+    SendCmd(mode);
+  }
+}
+
+//=======================================+抓取及放置函数+=================================
+void OrderSet(String str){
+  int n = str.toInt();
+  order[0] = n/10;
+  order[1] = n%10;
+}
+//==================================+二维码检测+=====================================
+/*
+  二维码识别函数
+  机械臂控制摄像头对准二维码
+  然后发送检测命令至上位机
+  等待接收上位机返回的数据(抓取顺序, 即 '12'的随机组合)
+  将数据在OLED上显示
+*/
+void QRDetect(){
+  ServoSet(servo_num, 73, 30, 64, 2, 82); //1.移动摄像头至二维码屏幕
+  data = SendCmd(QR); //获取上位机二维码识别结果mode=QR:检测二维码
+   LcdDisplayer(data);
+   //  LcdDisplay("hello" );
+   delay(1000);
+   ServoSet(servo_num, 73, 112, 31, 113, 90); //2.机械臂复位
+    delay(1000);
+   
+}
+
---
+++ stepper.ino
@@ -0,0 +1,321 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+//===============================+PIN definitions+===============================
+//enable pin
+#define EN_PIN 8
+//direction pins
+#define DIR_PIN_0 5
+#define DIR_PIN_1 6
+#define DIR_PIN_2 7
+#define DIR_PIN_3 13
+//directions
+#define MFORWARD_0 LOW
+#define MFORWARD_1 HIGH
+#define MFORWARD_2 LOW
+#define MFORWARD_3 HIGH
+//pulse pins
+#define PUL_PIN_0 2
+#define PUL_PIN_1 3
+#define PUL_PIN_2 4
+#define PUL_PIN_3 12
+//===============================-PIN definitions-===============================
+
+//===============================+imer definitions+=============================
+#define TIMER1 1
+#define TIMER3 3
+#define TIMER4 4
+#define TIMER5 5
+
+#define SYSTEM_CORE_CLOCK 16000000.0
+//===============================-timer definitions-===============================
+
+//===============================+robot config+===============================
+#define D_WHEEL 0.06;
+const double CWheel = M_PI * D_WHEEL;
+
+#define MAIN_STEP 200
+#define MICRO_STEP 8
+#define TOTAL_STEP (MAIN_STEP * MICRO_STEP)
+
+const double VRatio = (double)(2 * TOTAL_STEP) / CWheel; // 100 = TIMER_LIMIT / 1000000(ns per s)
+const double VYRatio = -1.25;   //修改此参数调节平移速度
+const double WRatio = 0.88 * 2000.0 * 0.04 / M_PI * 180.0; //0.88 修改此参数调节旋转速度
+// const double WRatio = 0.88 * 2000000.0 * 0.04 / M_PI * 180.0;
+
+#define MAX_VELOCITY 0.4  //limit:0.4
+#define MAX_OMEGA 1.0
+
+inline void run0(double v0);
+inline void run1(double v1);
+inline void run2(double v2);
+inline void run3(double v3);
+
+struct XY_LAST_STATUS
+{
+  unsigned long t_millis;
+  double vx;
+  double vy;
+  double w;
+}lastStat;
+//===============================-robot config-===============================
+
+//init method
+void initMotors(){
+  pinMode(EN_PIN, OUTPUT);
+  digitalWrite(EN_PIN, LOW);
+  
+  pos.x = 0;
+  pos.y = 0;
+  pos.theta = 0;
+  lastStat.t_millis = 0;
+  lastStat.vx = 0;
+  lastStat.vy = 0;
+  lastStat.w = 0;
+  
+  pinMode(DIR_PIN_0, OUTPUT);
+  pinMode(DIR_PIN_1, OUTPUT);
+  pinMode(DIR_PIN_2, OUTPUT);
+  pinMode(DIR_PIN_3, OUTPUT);
+  
+  pinMode(PUL_PIN_0, OUTPUT);
+  pinMode(PUL_PIN_1, OUTPUT);
+  pinMode(PUL_PIN_2, OUTPUT);
+  pinMode(PUL_PIN_3, OUTPUT);
+  
+  digitalWrite(DIR_PIN_0, MFORWARD_0);
+  digitalWrite(DIR_PIN_1, MFORWARD_1);
+  digitalWrite(DIR_PIN_2, MFORWARD_2);
+  digitalWrite(DIR_PIN_3, MFORWARD_3);
+  
+  digitalWrite(PUL_PIN_0, LOW);
+  digitalWrite(PUL_PIN_1, LOW);
+  digitalWrite(PUL_PIN_2, LOW);
+  digitalWrite(PUL_PIN_3, LOW);
+}
+
+//===============================+position API+===============================
+void updatePos() 
+{ 
+  double dt = millis() - lastStat.t_millis;
+  dt /= 1000.0;
+  lastStat.t_millis = millis();
+
+  double lastTheta = pos.theta;
+  pos.theta += dt * lastStat.w;
+  pos.theta -= double(floor(pos.theta / 2.0 / M_PI)) * 2.0 * M_PI;
+
+  if(lastStat.w == 0) {
+    pos.x += dt * (lastStat.vx * cos(lastTheta) - lastStat.vy * sin(lastTheta));
+    pos.y += dt * (lastStat.vy * cos(lastTheta) + lastStat.vx * sin(lastTheta));
+  }
+  else {
+    double v = sqrt(lastStat.vx * lastStat.vx + lastStat.vy * lastStat.vy);
+    double r = v / lastStat.w;
+  
+    pos.x += r * (sin(pos.theta) - sin(lastTheta));
+    pos.y += r * (-cos(pos.theta) + cos(lastTheta));
+  }
+  
+  pos.x = 0;
+  pos.y = 0;
+  pos.theta = 0;
+  lastStat.t_millis = 0;
+  lastStat.vx = 0;
+  lastStat.vy = 0;
+  lastStat.w = 0;
+}
+//===============================-position API-===============================
+
+//===============================+robot control+===============================
+//vx: m/s; vy: m/s; w: rad/s
+void XYRun(double vx, double vy, double w, int dir)
+{
+  double v = sqrt(vx*vx+vy*vy);
+  if(v > MAX_VELOCITY || w > MAX_OMEGA) return;
+  double vy_t = vy * VYRatio;
+  double v0 = VRatio * (vx+vy_t);
+  double v1 = VRatio * (vx-vy_t);
+  double w_t = WRatio * w;
+  
+  updatePos();
+  lastStat.vx = vx;
+  lastStat.vy = vy;
+  lastStat.w = w;
+  
+  double v_lf = (v0 - w_t); //左前
+  double v_rf = (v1 + w_t); //右前
+  double v_lb = (v1 - w_t); //左后
+  double v_rb = (v0 + w_t); //右后
+  
+  switch(dir){
+    case MOVE_LEFT: break;
+    case MOVE_RIGHT: break;
+    case TURN_LEFT: v_rb = 0; v_rf *= 0.2; break;
+    case TURN_RIGHT: v_lf = 0; v_rf *= 0.2; break;
+  }
+ 
+  run0(v_lf); run1(v_rf); run2(v_lb); run3(v_rb);
+}
+
+//v: m/s; dir: rad; w: rad/s
+inline void XYRunVDW(double v, double dir, double w) {XYRun(v*cos(dir),v*sin(dir),w,0);}
+
+inline void XYStop() {XYRun(0,0,0,0);}
+
+//步进电机使能开启
+inline void StepperEnable(){digitalWrite(EN_PIN, LOW);};
+
+//步进电机使能关闭
+inline void StepperDisable(){digitalWrite(EN_PIN, HIGH);};
+//===============================-robot control-===============================
+
+//===============================-velocity settings-===============================
+inline void run0(double v0) {
+  if(v0 < 0) {
+    digitalWrite(DIR_PIN_0, MFORWARD_0 ^ 1);
+    TimerStart(TIMER1, -v0);
+  } else if(v0 == 0) {
+    TimerStop(TIMER1);
+  } else {
+    digitalWrite(DIR_PIN_0, MFORWARD_0);
+    TimerStart(TIMER1, v0);
+  }
+}
+inline void run1(double v1) {
+  if(v1 < 0) {
+    digitalWrite(DIR_PIN_1, MFORWARD_1 ^ 1);
+    TimerStart(TIMER3, -v1);
+  } else if(v1 == 0) {
+    TimerStop(TIMER3); 
+  } else {
+    digitalWrite(DIR_PIN_1, MFORWARD_1);
+    TimerStart(TIMER3, v1);
+  }
+}
+inline void run2(double v2) {
+  if(v2 < 0) {
+    digitalWrite(DIR_PIN_2, MFORWARD_2 ^ 1);
+    TimerStart(TIMER4, -v2);
+  } else if(v2 == 0) { 
+    TimerStop(TIMER4);
+  } else {
+    digitalWrite(DIR_PIN_2, MFORWARD_2);
+    TimerStart(TIMER4, v2);
+  }
+}
+inline void run3(double v3) {
+  if(v3 < 0) {
+    digitalWrite(DIR_PIN_3, MFORWARD_3 ^ 1);
+    TimerStart(TIMER5, -v3);
+  } else if(v3 == 0) {
+    TimerStop(TIMER5);
+  } else {
+    digitalWrite(DIR_PIN_3, MFORWARD_3);
+    TimerStart(TIMER5, v3);
+  }
+}
+//===============================-velocity settings-===============================
+
+//====================================+timer+======================================
+
+void TimerStart(int which, double frequency){
+  double _RC = SYSTEM_CORE_CLOCK / 8.0 / frequency - 1;
+  cli();
+  switch(which){
+    case TIMER1: 
+      TCCR1A = 0; TCCR1B = 0; 
+      OCR1A = _RC;
+      //TCCR1B = _BV(WGM12) | _BV(CS10) | _BV(CS11); 
+      TCCR1B = _BV(WGM12) | _BV(CS11); 
+      TIMSK1 |= (1 << OCIE1A); 
+      break;
+    case TIMER3: 
+      TCCR3A = 0; TCCR3B = 0; 
+      OCR3A = _RC;
+      //TCCR3B = _BV(WGM12) | _BV(CS10) | _BV(CS11); 
+      TCCR3B = _BV(WGM12) | _BV(CS11); 
+      TIMSK3 |= (1 << OCIE3A); 
+      break;
+    case TIMER4: 
+      TCCR4A = 0; TCCR4B = 0; 
+      OCR4A = _RC;
+      //TCCR4B = _BV(WGM12) | _BV(CS10) | _BV(CS11); 
+      TCCR4B = _BV(WGM12) | _BV(CS11); 
+      TIMSK4 |= (1 << OCIE4A); 
+      break;
+    case TIMER5: 
+      TCCR5A = 0; TCCR5B = 0; 
+      OCR5A = _RC;
+      //TCCR5B = _BV(WGM12) | _BV(CS10) | _BV(CS11); 
+      TCCR5B = _BV(WGM12) | _BV(CS11); 
+      TIMSK5 |= (1 << OCIE5A); 
+      break;
+  }
+  sei();
+}
+
+void TimerStop(int which){
+  switch(which){
+    case TIMER1: TCCR1B = 0; break;
+    case TIMER3: TCCR3B = 0; break;
+    case TIMER4: TCCR4B = 0; break;
+    case TIMER5: TCCR5B = 0; break;
+  }
+}
+
+ISR(TIMER1_COMPA_vect){
+  static byte _state = 0;
+  static byte _pin = PUL_PIN_0;
+  digitalWrite(_pin, _state ^= 1);
+}
+ISR(TIMER3_COMPA_vect){
+  static byte _state = 0;
+  static byte _pin = PUL_PIN_1;
+  digitalWrite(_pin, _state ^= 1);
+}
+ISR(TIMER4_COMPA_vect){
+  static byte _state = 0;
+  static byte _pin = PUL_PIN_2;
+  digitalWrite(_pin, _state ^= 1);
+}
+ISR(TIMER5_COMPA_vect){
+  static byte _state = 0;
+  static byte _pin = PUL_PIN_3;
+  digitalWrite(_pin, _state ^= 1);
+}
+//====================================-timer-======================================
+
+//====================================+move+======================================
+/*
+  车移动函数
+  参数:
+    x: 车在x轴方向上移动的距离
+    y: 车在y轴方向上移动的距离
+    _loop: true:以一定速度一直转动 ; false: 转动指定距离
+    dir: 车移动方向
+*/
+void moveTo(double x, double y, boolean _loop, int dir)
+{
+  updatePos();
+  //logPosition();
+  double dx = x - pos.x;
+  double dy = y - pos.y;
+  //if(!dx&&!dy)return;
+  double vx = dx > 0 ? 0.1 : -0.1;
+  double vy = dy > 0 ? 0.1 : -0.1; 
+  double tx = dx / vx;
+  double ty = dy / vy;
+  double t_double = tx > ty ? tx : ty;
+  unsigned long t = ceil(t_double*1000.0);
+  if(t==0) return;
+  vx = dx / (double)t * 1000.0;
+  vy = dy / (double)t * 1000.0;
+  XYRun(vx, vy, 0, dir);
+  
+  if(!_loop){
+    delay(t);
+    XYStop();
+  }
+}
+//====================================-move-======================================
